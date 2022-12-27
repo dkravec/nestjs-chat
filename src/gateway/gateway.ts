fdebc3d8b5b258e25c89945cf0ea4dc4e9f6ebb5
Daniel Kravec <daniel@kravec.com>
2022-12-27 21:54:42 +0000

    working back and forth

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,18 +1,37 @@
+import { OnModuleInit } from '@nestjs/common';
 import {
   SubscribeMessage,
   WebSocketGateway,
   MessageBody,
+  WebSocketServer,
 } from '@nestjs/websockets';
-
+import { Server } from 'socket.io';
 @WebSocketGateway()
 // @WebSocketGateway({
 //     cors: {
 //         origin: "blank"
 //     }
 // })
-export class MyGateway {
+export class MyGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+    });
+    // this.server.on('newMessage', (body) => {
+    //   console.log(body);
+    // });
+  }
+
   @SubscribeMessage('newMessage')
   onNewMessage(@MessageBody() body: any) {
-    console.log(body);
+    // console.log(this.server.sockets.);
+    console.log('newMessage' + body);
+    this.server.emit('onMessage', {
+      msg: 'new message',
+      content: body,
+    });
   }
 }
