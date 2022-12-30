b965e6eb9ec00da0bd47ec94601529533657f6d2
Daniel Kravec <daniel@kravec.com>
2022-12-30 04:20:05 +0000

    added a namespace, can now read client info from message recieve, adds the clients into an object will be used to view everyone online

--- src/gateway/chat.gateway.ts
+++ src/gateway/chat.gateway.ts
@@ -7,8 +7,9 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   OnGatewayInit,
+  ConnectedSocket,
 } from '@nestjs/websockets';
-import { Namespace, Server } from 'socket.io';
+import { Namespace, Server, Socket } from 'socket.io';
 import { Message, Connection } from '../types/types';
 // @WebSocketGateway({
 //     cors: {
@@ -17,18 +18,15 @@ import { Message, Connection } from '../types/types';
 // })
 
 const messages: Message[] = [];
-const conections: Connection = {
-  socket_id: 'abc',
-  user_id: 214,
-};
-// console.log(conections);
 
-@WebSocketGateway()
+@WebSocketGateway({
+  namespace: 'chat', // connects to /chat
+})
 //  OnModuleInit, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 export class ChatGateway implements OnModuleInit {
-  @WebSocketServer() // io: Namespace;
+  @WebSocketServer() //io: Namespace;
   server: Server;
-
+  connections: any = {};
   onModuleInit() {
     this.server.on('connection', (socket) => {
       console.log(socket.id);
@@ -36,7 +34,10 @@ export class ChatGateway implements OnModuleInit {
   }
 
   @SubscribeMessage('newMessage')
-  onNewMessage(@MessageBody() body: any) {
+  onNewMessage(@ConnectedSocket() client: Socket, @MessageBody() body: any) {
+    console.log(`Client ID: ${client.id}`);
+    this.server.to(client.id).emit('sendback');
+
     console.log('newMessage');
     messages.push(body);
     console.log(messages);
@@ -48,11 +49,27 @@ export class ChatGateway implements OnModuleInit {
   }
 
   @SubscribeMessage('userLogin')
-  onUserLogin(@MessageBody() body: any) {
+  onUserLogin(@ConnectedSocket() client: Socket, @MessageBody() body: any) {
     // console.log(this.server.sockets.server);
     // if (!body.userId) this.server.to()
     messages.push(body);
     console.log(messages);
+    /*
+      {
+        "id" : 1,
+        "content" : "This is a test",
+        "user_id" : 2,
+        "user_email" : "test@tester.com"
+      }
+    */
+    this.connections[`${body.user_email}`] = {
+      socket_id: client.id,
+      user_id: body.user_id,
+    };
+    console.log(this.connections);
+    //  Connection = {
+
+    // };
     this.server.emit('onMessage', {
       msg: 'new message',
       content: body,
---
+++ src/types/Group.ts
@@ -0,0 +1,16 @@
+/*
+id
+users [
+  1
+  2
+  3
+]
+index 
+  -> 
+    indexid
+    messages[]
+  ->
+    messageid
+    content 
+    timestamp 
+*/
